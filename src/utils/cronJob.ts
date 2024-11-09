9e86b0572187b0acb5ff5c714e8118e26ea8ac47
Abhishek P P <abhishekpp.bluecast@gmail.com>
2024-11-09 19:55:16 +0530

    removes ngrok setup form the server

--- src/utils/cronJob.ts
+++ src/utils/cronJob.ts
@@ -17,7 +17,6 @@ export const cronJob = new CronJob("*/1 * * * *", async () => {
 
     // Admin Meta app access token fetching.
     await getAppAccessToken();
-    await subscribeWebhook();
     
     // Admin Meta webhook subscription if not subscribed.
     if(!await checkWebhookSubscription()) {
--- src/utils/socialMediaUtility.ts
+++ src/utils/socialMediaUtility.ts
@@ -5,7 +5,7 @@ import { AdminFacebookSettings } from '../socialMedia/dataModels/entities/adminF
 import { admins } from '../users/admin/dataModels/entities/admin.entity';
 import { getDataSource } from './dataSource';
 import { FacebookWebhookRequest } from '../socialMedia/dataModels/types/meta.types';
-import { ngrokUrl } from '../server';
+// import { ngrokUrl } from '../server';
 
 // Social Media Utility Constants
 export const CLIENT_URL = process.env.FRONTEND_URL as string;
@@ -30,7 +30,7 @@ export const verifySignature = (signature: string | undefined, body: any, appSec
 
   // Convert body to a JSON string for hashing
   const bodyString = typeof body === 'string' ? body : JSON.stringify(body);
-  
+
   const expectedHash = crypto.createHmac('sha256', appSecret).update(bodyString).digest('hex');
   return signatureHash === expectedHash;
 };
@@ -141,8 +141,8 @@ export const subscribeWebhook = async () => {
     if(adminSocialMediaData) {
       const appId = process.env.META_APP_ID;
       const verifyToken = process.env.META_APP_VERIFY_TOKEN;
-      // const callbackUrl = process.env.NGROK_URL +'/api/v1/meta/webhook';      
-      const callbackUrl = ngrokUrl + '/api/v1/meta/webhook';
+      const callbackUrl = process.env.NGROK_URL +'/api/v1/meta/webhook';      
+      // const callbackUrl = ngrokUrl + '/api/v1/meta/webhook';
       const appAccessToken = adminSocialMediaData.facebook.appAccessToken;
 
       const url = `https://graph.facebook.com/v20.0/${appId}/subscriptions?access_token=${appAccessToken}`;
