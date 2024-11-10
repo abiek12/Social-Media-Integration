ee8e3865a80d80f7ac6eef6cc07a9c10a66eb4dc
Abhishek P P <abhishekpp.bluecast@gmail.com>
2024-11-10 11:32:20 +0530

    adds cors

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,14 @@ dotenv.config();
 
 const app: Application = express();
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
