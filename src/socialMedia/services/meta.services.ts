ec5eaecbf0ad08b4255ff9b94f5afa1b36223da0
Abhishek P P <abhishekpp.bluecast@gmail.com>
2024-11-20 14:55:07 +0530

    removes unwanted logs

--- src/socialMedia/services/meta.services.ts
+++ src/socialMedia/services/meta.services.ts
@@ -27,8 +27,6 @@ export class metaServices {
             const signature = request.headers['x-hub-signature'] as string | undefined;
             const body = request.body;
             console.log(body);
-            console.log(body.entry[0].changes);
-            console.log(body.entry[0].changes[0]);
         
             const appSecret = process.env.META_APP_SECRET;
             if (!appSecret) {
